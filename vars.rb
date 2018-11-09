c7d59e1f5346ac6af1a28ce037d1af286f950563
System Administrator <root@not-my-mac.local>
2018-11-09 21:34:42 +0100

    initial commit

---
+++ vars.rb
@@ -0,0 +1,28 @@
+@current_btc_price = "6500"
+
+@coincode_short = "eth"
+@coincode_long = "Ethereum"
+
+@algorithm = "Ethash"
+@denominator = "MH/s"
+
+@bronze_price="0.0090"
+@bronze_hash="200"
+
+@silver_price="0.0170"
+@silver_hash="400"
+
+@gold_price="0.0300"
+@gold_hash="600"
+
+@platinum_price="0.0550"
+@platinum_hash="1400"
+
+@profit_single_hash="0.4796"
+
+## dont change under this is not necessarily
+@site_name = "#{@coincode_short} mining"
+@site_title = "start #{@site_name} in our cloud infrastructure today!"
+@description = "#{@site_name} offers #{@coincode_short} cloud mining at competitive price. Start mining #{@coin_long} today!"
+@keywords = "#{@coincode_short} mining, #{@coincode_long} mining, cryptocurrency mining, #{@site_name}, gpu mining, asic mining, cpu mining, cloud mining"
+@author = "#{@site_name}"
